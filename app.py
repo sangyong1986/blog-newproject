1e611a1f8dcd1a7f48fbb6c117b8422aa3f29aeb
sangyong1986 <sangyong1986@gmail.com>
2026-06-27 13:47:07 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
+import os
 
-app = Flask(__name__)
+app = Flask(__name__,
+    template_folder="templates",
+    static_folder="static",
+    static_url_path="/static"
+)
 
 posts = [
     {
